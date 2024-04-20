eefb96a1a9e8d5f67f3e3a0593091ca18eb6af06
AbraaoAlves <abraao.teodosio@gmail.com>
2024-04-20 17:39:21 +0000

    start java project

---
+++ app/src/test/java/com/atv1/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atv1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
